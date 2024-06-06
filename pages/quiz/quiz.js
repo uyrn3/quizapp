02c29a1a96156cd65b8b2890111f21f5a9055f71
Eduardo Rosa <emeira127@gmail.com>
2024-06-06 05:19:41 +0000

    validar a resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -9,6 +9,7 @@ let pontos = 0
 let pergunta = 1
 let resposta  = ""
 let idInputResposta = ""
+let respostaCorretaId = ""
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -105,6 +106,19 @@ function alterarSinais(texto) {
 function guardarResposta(evento) {
     resposta = evento.target.value
     idInputResposta = evento.target.id
+
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("CLICK", validarResposta)
+}
+
+function validarResposta() {
+    if (resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='$(idInputResposta)']`).setAttribute("id", "correta")
+        pontos = pontos + 1
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
 }
 
 async function iniciar() {
@@ -115,6 +129,10 @@ async function iniciar() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.question[pergunta-1].answer) {
+            respostaCorretaId = input.id
+        }
     })
 }
 
