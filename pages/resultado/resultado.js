f4b2e0d52169da1fbcea86b306236863afca6779
Eduardo Rosa <emeira127@gmail.com>
2024-06-06 05:25:36 +0000

    funcao de inserir resultado

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -1,10 +1,41 @@
-import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
+
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeimg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeimg.setAttribute("src", `../../assets/images/icon-${assunto.toLocaleLowerCase()}.svg`)
+    iconeimg.setAttribute("alt", `icone de ${assunto}`)
+    assuntoTitulo.innerHTML = assunto
+}
+
+alterarAssunto()
+
+function inserirResultado() {
+    const sectionPontuacao = document.querySelector(".pontuacao")
+    const divAssunto = document.querySelector(".assunto")
+    const pontos = localStorage.getItem("pontos")
+
+    sectionPontuacao.innerHTML = `
+        ${divAssunto.outerHTML}
+
+        <strong>${pontos}</strong>
+
+        <p>de 10</p>
+    `
+}
+
+inserirResultado()
